2616f268e562ed5351d8cf1358bd066b842fc3d7
hasin.pramodya <pramodyahasin@gmail.com>
2025-09-01 16:00:00 +0530

    implement fibonacci sequence

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,21 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static long [] fibonaacciChace ;
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       int number = 6; // Example: Calculate the 10th Fibonacci number
+        fibonaacciChace = new long[number + 1]; // Initialize the cache array
+        System.out.println(fibonacci(number));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static long fibonacci(int n){
+        if(n<=1){
+            return n;  // if n is 0 or 1 return the value of n and terminate the method
         }
+        if(fibonaacciChace[n] != 0){
+            return fibonaacciChace[n]; // return the cached value if it exists
+        }
+        fibonaacciChace [n] = fibonacci(n-1) + fibonacci(n-2);
+        return fibonaacciChace[n];// recursive call
     }
 }
